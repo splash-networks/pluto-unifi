c67e2befd7679a42dbf19f2d5ae969f38298bf24
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2024-02-06 11:43:50 +0500

    Add new design

--- vee2oye1/index.php
+++ vee2oye1/index.php
@@ -6,25 +6,25 @@ $_SESSION["id"] = $_GET['id'];
 $_SESSION["ap"] = $_GET['ap'];
 
 if (isset($_POST['connect'])) {
-  $mac = $_SESSION["id"];
-  $apmac = $_SESSION["ap"];
-  $url = $_SERVER['REDIRECT_URL'];
+    $mac = $_SESSION["id"];
+    $apmac = $_SESSION["ap"];
+    $url = $_SERVER['REDIRECT_URL'];
 
-  $controlleruser = $_SERVER['CONTROLLER_USER'];
-  $controllerpassword = $_SERVER['CONTROLLER_PASSWORD'];
-  $controllerurl = $_SERVER['CONTROLLER_URL'];
-  $controllerversion = $_SERVER['CONTROLLER_VERSION'];
-  $duration = $_SERVER['DURATION'];
-  $debug = false;
-  $site_id = $_SERVER['SITE_ID'];
+    $controlleruser = $_SERVER['CONTROLLER_USER'];
+    $controllerpassword = $_SERVER['CONTROLLER_PASSWORD'];
+    $controllerurl = $_SERVER['CONTROLLER_URL'];
+    $controllerversion = $_SERVER['CONTROLLER_VERSION'];
+    $duration = $_SERVER['DURATION'];
+    $debug = false;
+    $site_id = $_SERVER['SITE_ID'];
 
-  $unifi_connection = new UniFi_API\Client($controlleruser, $controllerpassword, $controllerurl, $site_id, $controllerversion);
-  $set_debug_mode = $unifi_connection->set_debug($debug);
-  $loginresults = $unifi_connection->login();
+    $unifi_connection = new UniFi_API\Client($controlleruser, $controllerpassword, $controllerurl, $site_id, $controllerversion);
+    $set_debug_mode = $unifi_connection->set_debug($debug);
+    $loginresults = $unifi_connection->login();
 
-  $auth_result = $unifi_connection->authorize_guest($mac, $duration, null, null, null, $apmac);
+    $auth_result = $unifi_connection->authorize_guest($mac, $duration, null, null, null, $apmac);
 
-  header("Location: $url");
+    header("Location: $url");
 }
 
 ?>
@@ -34,45 +34,34 @@ if (isset($_POST['connect'])) {
 <head>
     <meta charset="utf-8">
     <title>
-      <?php echo htmlspecialchars($business_name); ?> WiFi</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <link rel="stylesheet" href="../assets/styles/bulma.min.css"/>
-    <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css"/>
-    <link rel="stylesheet" href="../assets/styles/style.css"/>
+        <?php echo htmlspecialchars($business_name); ?> WiFi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="../assets/styles/bulma.min.css" />
+    <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
+    <link rel="stylesheet" href="../assets/styles/style.css" />
 </head>
 
 <body>
-<div class="page">
-
-    <div class="head">
-        <br>
-        <figure id="logo">
-            <img src="../assets/images/logo.png">
-        </figure>
-    </div>
-
-    <div class="main">
-        <section class="section">
-            <div class="container">
-                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                    <div class="field">
-                        <div class="control has-icons-left">
-                            <input class="input" type="password" name="pass" placeholder="Password" required>
-                            <span class="icon is-small is-left">
-                              <i class="fas fa-key"></i>
-                            </span>
+    <div class="container">
+        <main>
+            <div class="is-flex-direction-column is-justify-content-space-evenly full-height">
+                <section>
+                    <figure class="image is-square">
+                        <img src="../assets/images/logo.png">
+                    </figure>
+                </section>
+                <section>
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                        <p class="has-text-centered">Enjoy free WiFi</p>
+                        <br>
+                        <div class="buttons is-centered">
+                            <input class="button is-dark is-responsive is-fullwidth" type="submit" name="connect" value="Login">
                         </div>
-                    </div>
-                    <br>
-                    <div class="buttons is-centered">
-                        <input class="button" type="submit" name="connect" value="Connect">
-                    </div>
-                </form>
+                    </form>
+                </section>
             </div>
-            <br>
-        </section>
+        </main>
     </div>
-</div>
 </body>
 
 </html>
