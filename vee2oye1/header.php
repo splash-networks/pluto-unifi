29244f40eb2532965ad5bb77d73f480eea90f6f5
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2024-02-06 11:46:25 +0500

    Uncomment code

--- vee2oye1/header.php
+++ vee2oye1/header.php
@@ -2,13 +2,13 @@
 
 session_start();
 
-// if (!(isset($_SESSION['id']) or isset($_GET['id']))) {
-//   exit('This page cannot be accessed directly. It only works when using a hotspot.');
-// }
+if (!(isset($_SESSION['id']) or isset($_GET['id']))) {
+    exit('This page cannot be accessed directly. It only works when using a hotspot.');
+}
 
-// require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-// $dotenv->load();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
 
-// $business_name = $_SERVER['BUSINESS_NAME'];
+$business_name = $_SERVER['BUSINESS_NAME'];
